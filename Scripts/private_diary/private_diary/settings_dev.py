1369170d4ca4e31f60dd95e11e37d78f77e6c215
tomoakiitoh <tomoaki.itoh@gmail.com>
2022-05-07 13:11:42 +0900

    Chapter11まで終わったところ

---
+++ Scripts/private_diary/private_diary/settings_dev.py
@@ -0,0 +1,54 @@
+from private_diary.settings_common import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '$5^7&ejfv38&!y2khu@r32k0@7ph-1p4i&)@@_&je@f2#6&$eq'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# ロギング設定
+LOGGING = {
+    'version': 1,  # 1固定
+    'disable_existing_loggers': False,
+
+    # ロガーの設定
+    'loggers': {
+        # Djangoが利用するロガー
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        # diaryアプリケーションが利用するロガー
+        'diary': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+
+    # ハンドラの設定
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'dev'
+        },
+    },
+
+    # フォーマッタの設定
+    'formatters': {
+        'dev': {
+            'format': '\t'.join([
+                '%(asctime)s',
+                '[%(levelname)s]',
+                '%(pathname)s(Line:%(lineno)d)',
+                '%(message)s'
+            ])
+        },
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
